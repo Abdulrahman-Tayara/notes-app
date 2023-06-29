31be9c3ac95f5085f0e20188b21a53c8f1998058
abdulrahman <abdulrahman@averroes.ai>
2023-06-29 15:41:43 +0400

    design signup page

--- web-app/src/features/auth/signup/SignUpPage.tsx
+++ web-app/src/features/auth/signup/SignUpPage.tsx
@@ -1,36 +1,73 @@
-import React, { useCallback } from "react";
+import React, {useCallback} from "react";
 import useViewModel from "./viewmodel";
+import {Avatar, Box, Button, Container, CssBaseline, Grid, Link, TextField, Typography} from "@mui/material";
+import {LockOutlined} from "@mui/icons-material";
+import {Routes} from "common/routes/constants";
 
 const SignUpPage = () => {
-  const { name, email, password, signUp } = useViewModel();
+    const {name, email, password, signUp} = useViewModel();
 
-  const handleSubmit = useCallback(() => {
-    signUp()
-  }, [signUp]);
+    const handleSubmit = useCallback(() => {
+        signUp()
+    }, [signUp]);
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={name.value}
-        onChange={(e) => name.setter(e.target.value)}
-      />
-      <br></br>
-      <input
-        type="email"
-        value={email.value}
-        onChange={(e) => email.setter(e.target.value)}
-      />
-      <br></br>
-      <input
-        type="password"
-        value={password.value}
-        onChange={(e) => password.setter(e.target.value)}
-      />
-      <br></br>
-      <button onClick={(e) => handleSubmit()}>Sign up</button>
-    </div>
-  );
+    return (
+        <Container component="main" style={{alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
+            <CssBaseline/>
+            <Box sx={{
+                padding: 8,
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                boxShadow: 10,
+                width: "50%",
+                borderRadius: '0.5rem'
+            }}>
+                <Avatar sx={{bgcolor: 'secondary.main'}}>
+                    <LockOutlined/>
+                </Avatar>
+
+                <Typography component="h1" variant="h5" sx={{marginTop: '5%'}}>
+                    Sign up
+                </Typography>
+
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    autoFocus/>
+
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"/>
+
+                <Button type='submit'
+                        fullWidth
+                        sx={{marginTop: 8}}
+                        variant='contained'>
+                    Sign Up
+                </Button>
+
+
+                <Link href={Routes.Login} sx={{marginTop: '5%'}}>
+                    Already have an account?
+                </Link>
+
+            </Box>
+        </Container>
+    );
 };
 
 
